385098748a7fa16d19060eca42706d7111ff05b2
Sharif10123 <137716658+Sharif10123@users.noreply.github.com>
2024-06-25 16:13:14 +0000

    Add files via upload

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+            Console.WriteLine($"Ticket Price: GHS{ticketPrice}");
+        }
+    }
+}
